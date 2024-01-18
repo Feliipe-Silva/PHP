dede9bd05159aa310f08b6d9bf5c6d3495c4c2d7
Felipe Silva <150200576+Feliipe-Silva@users.noreply.github.com>
2024-01-18 15:14:06 -0300

    7º-Desafio proposto feito!
    Feito!

--- DESAFIOS/7º-Desafio/desafio07.php
+++ DESAFIOS/7º-Desafio/desafio07.php
@@ -9,14 +9,14 @@
 <body>
     <?php 
         $sa = $_POST['sa'] ?? 0;
-        //$minimo = 1380;
+        $minimo = 1_412.00; // valor do salário mínimo de 2024;
     ?>
     <main>
         <h1>Informe o seu salário</h1>
         <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post">
             <label for="">Salário R$:</label>
                 <input type="number" name="sa" id="sa">
-            <p>Considerando o salário mínimo de <strong>R$: 1.380,00</strong></p>
+            <p>Considerando o salário mínimo de <strong>R$: <?=number_format($minimo, 2, ".", ","); ?></strong></p>
                 <input type="submit" value="Calcular">
             
         </form>
@@ -25,9 +25,9 @@
     <section>
         <h2>Resultado Final</h2>
         <?php
-            $minimo = 1380;
-            $resto = $sa % $minimo;
-            print "Quem recebi um salário de $sa ganha + $resto";
+            $resu = intdiv($sa, $minimo); //Resultado do valor inteiro, do (valor input dividido do salário mínimo);
+            $resto = $sa % $minimo; //Aqui o resultado do resto da divisão;
+            print "Quem recebi um salário de R\$" . number_format($sa, 2, ".", ","). ", ganha <strong>$resu salário mínimo +$resto reais.</strong>";
         ?>
     </section>
 </body>
