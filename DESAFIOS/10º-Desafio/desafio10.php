d468efd05e4c51a5fbd6bb41e7646c27167b9456
Felipe Silva <150200576+Feliipe-Silva@users.noreply.github.com>
2024-02-07 14:23:07 -0300

    Calculadora de idade/ano

--- DESAFIOS/10º-Desafio/desafio10.php
+++ DESAFIOS/10º-Desafio/desafio10.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Desafio 10</title> 
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="desafio10.css">
 </head>
 <body>
     <?php
@@ -16,18 +16,29 @@
         <h1>Calculadora de Idade</h1>
         <form action="<?=$_SERVER['PHP_SELF']; ?>" method="get">
             <label for="">Em que ano nasceu?</label>
-                <input type="number" name="anoN" id="anoN">
+                <input type="number" name="anoN" id="anoN" max="<?=$anoT?>">
             <label for="">Que saber sua idade em que ano? (atualmente estamos em 2024)</label>
                 <input type="number" name="anoT" id="anoT" value="2024">
                 <input type="submit" value="Qual será a sua idade?">
         </form>
     </main>
     <section>
-        <h2>Sua Idade</h2>
+        <h2>Sua Idade:</h2>
         <?php
-            $res = $anoT - $nasceu;
-            print "$data";
-            print "<p>Você tem idade de $res anos.</p>";
+            $res = $anoT - $nasceu;       
+                    print "Atualmente estamos em $data.";
+                    print "<p>Você tem idade de <strong>$res anos.</strong></p>";
+
+            
+              /*  if ($date == 2024) {
+                    print "Atualmente estamos em $data.";
+                    print "<p>Você tem idade de <strong>$res anos.</strong></p>";
+
+                } else if ($date > 2024) {
+                    print "Com base no ano de $anoT.";
+                    print "<p>Você tem idade de <strong>$res anos.</strong></p>";
+                };    */
+                
         ?>
     </section>
 </body>
